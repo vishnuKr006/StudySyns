1ecc79adf117aa5b77f6efcfd15146616968b83a
vishnuKr006 <162488939+vishnuKr006@users.noreply.github.com>
2024-04-06 20:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let user = setTimeout(function () {
+    alert("Accept All Cookies")
+}, 8000)
+
+var typed = new Typed('#element', {
+    strings: ['Seamless Learning for Your <span class="Green">Brighter Futures</span>', 'Learn to code with StudySync', 'Our innovative platform offers an effortless and seamless approach to learning,', 'empowering students of all ages to achieve brighter futures.', 'Browse courses and find out the best course for you. Its free!', 'StudySync is my attempt to teach basics and those coding techniques to people in short time which took me ages to learn.', 'Seamless Learning for Your <span class="Green">Brighter Futures</span>'],
+    typeSpeed: 70,
+});
